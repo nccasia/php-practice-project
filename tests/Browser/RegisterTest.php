4fb60f33beda262d9f32bfbfbaec337bb5731b39
ConCongSan <112951971+ConCongSan@users.noreply.github.com>
2023-03-08 17:21:42 +0700

    Update dusk Register Test

---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    public function test_user_register(): void
+    {
+        $user = User::factory()->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/admin/register')
+                ->assertSee('Register')
+                ->type('username', $user->username)
+                ->type('email', $user->email)
+                ->type('name', $user->name)
+                ->type('password', $user->password)
+                ->type('Confirm_password', $user->password)
+                ->press('Register')
+                ->assertPathIs('/admin/login');
+        });
+    }
+}
